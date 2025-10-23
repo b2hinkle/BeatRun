bf48156581343e71cbccd20fa150a7eca3d224e3
Christian Hinkle <christianhinko@gmail.com>
2025-10-23 11:16:53 -0600

    [ui][cpp] Add button bus MultiHandler placeholder code for possibly handling button events in the future if converted to full c++.

--- Gem/Source/GameStates/StageSelect.cpp
+++ Gem/Source/GameStates/StageSelect.cpp
@@ -6,12 +6,25 @@
 #include <LyShine/Bus/UiCanvasManagerBus.h>
 #include <LyShine/Bus/UiCanvasBus.h>
 #include <Source/Utils/CanvasUtils.h>
+#include <LyShine/Bus/UiButtonBus.h>
 #endif // #if AZ_TRAIT_CLIENT
 #include <AzCore/Console/ILogger.h>
 #include <AzCore/Component/ComponentApplicationBus.h>
 
 namespace xXGameProjectNameXx::GameStates
 {
+#if AZ_TRAIT_CLIENT
+    void StageSelect::OnButtonClick()
+    {
+        const AZ::EntityId* currentBusIdPtr = UiButtonNotificationBus::GetCurrentBusId();
+        AZ_Assert(currentBusIdPtr, "This cannot be null since we are currently in a bus callback.");
+        [[maybe_unused]] const AZ::EntityId& currentBusId = *currentBusIdPtr;
+
+        // @Christian: TODO: [todo][ui] Use this to handle cpp button click event callbacks if we ever convert
+        // the UI canvas to cpp where we'd have to create each button individually. See: `LyShineExamplesCppExample` for reference.
+    }
+#endif // #if AZ_TRAIT_CLIENT
+
     void StageSelect::OnPushed()
     {
         GameState::IGameState::OnPushed();
@@ -49,10 +62,18 @@ namespace xXGameProjectNameXx::GameStates
     void StageSelect::OnEnter()
     {
         GameState::IGameState::OnEnter();
+
+#if AZ_TRAIT_CLIENT
+        UiButtonNotificationBus::MultiHandler::BusConnect(m_canvasEntityId);
+#endif // #if AZ_TRAIT_CLIENT
     }
 
     void StageSelect::OnExit()
     {
+#if AZ_TRAIT_CLIENT
+        UiButtonNotificationBus::MultiHandler::BusDisconnect(m_canvasEntityId);
+#endif // #if AZ_TRAIT_CLIENT
+
         GameState::IGameState::OnExit();
     }
 
--- Gem/Source/GameStates/StageSelect.h
+++ Gem/Source/GameStates/StageSelect.h
@@ -3,18 +3,32 @@
 
 #include <GameState/GameState.h>
 #include <AzCore/Component/EntityId.h>
+#if AZ_TRAIT_CLIENT
+#include <LyShine/Bus/UiButtonBus.h>
+#endif // #if AZ_TRAIT_CLIENT
 
 namespace xXGameProjectNameXx::GameStates
 {
     //! @class StageSelect
     class StageSelect
         : public GameState::IGameState
+#if AZ_TRAIT_CLIENT
+        , public UiButtonNotificationBus::MultiHandler
+#endif // #if AZ_TRAIT_CLIENT
     {
     public:
 
         AZ_CLASS_ALLOCATOR(GameStates::StageSelect, AZ::SystemAllocator);
 
         AZ_RTTI(GameStates::StageSelect, "{254EB57F-6827-45FF-9793-8742DC9DB542}", GameState::IGameState);
+
+    public:
+
+#if AZ_TRAIT_CLIENT
+        //! UiButtonNotificationBus::MultiHandler public overrides.
+        void OnButtonClick() override;
+        //! @}
+#endif // #if AZ_TRAIT_CLIENT
 
     protected:
 
